15050446f38da44d7f82156f7196ff8dcf32c279
Robert Turner <Robert_AFTurner@hotmail.com>
2020-06-10 12:49:22 +0100

    Initial paralax implementation

---
+++ SidescrollingShooter/SidescrollingShooter/Assets/Scripts/ParalaxBackgroundController.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class ParalaxBackgroundController : MonoBehaviour
+{
+    private float length;
+    private float startPosition;
+
+    [SerializeField]
+    private float paralaxEffectMultiplyer;
+
+    [SerializeField]
+    private GameObject camera;
+
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        var distanceToMove = camera.transform.position.x * paralaxEffectMultiplyer;
+        transform.position = new Vector2(startPosition + distanceToMove, camera.transform.position.y);
+
+        var tempValue = camera.transform.position.x * 1 - paralaxEffectMultiplyer;
+
+        if (tempValue > startPosition + length)
+        {
+            startPosition += length;
+        }
+        else if (tempValue < startPosition - length)
+        {
+            startPosition -= length;
+        }
+    }
+}
